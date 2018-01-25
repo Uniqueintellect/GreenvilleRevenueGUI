2a4aca492ba9bed8b0b1803627144d80de96904f
jl7164ut <33062505+jl7164ut@users.noreply.github.com>
2018-01-25 12:58:27 -0600

    weird

--- GreenvilleRevenueGUI/Form1.cs
+++ GreenvilleRevenueGUI/Form1.cs
@@ -17,6 +17,11 @@ namespace GreenvilleRevenueGUI
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
         private void outputButton_Click(object sender, EventArgs e)
         {
 
