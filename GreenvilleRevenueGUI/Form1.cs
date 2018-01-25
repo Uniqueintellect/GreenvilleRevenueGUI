7855aeb1bbe7054aa41dddeb5ad461386a60a38f
jl7164ut <33062505+jl7164ut@users.noreply.github.com>
2018-01-25 13:07:17 -0600

    added comment

--- GreenvilleRevenueGUI/Form1.cs
+++ GreenvilleRevenueGUI/Form1.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GreenvilleRevenueGUI
+/* Program written by: Zach, Jeff
+* Program date: 1/25/18
+* Program description: you enter 2 numbers, multiplies one of them and prints out a total, then compares the 2 numbers and prints out a statement. Chatper 3 case study 1, this is now in GUI form. You click buttons and textboxes to inout data.
+* Encountered issues: had a method that we couldn't delete.
+* */
 {
     public partial class greenvilleRevenueGUI : Form
     {
@@ -22,7 +27,7 @@ namespace GreenvilleRevenueGUI
 
         }
 
-        private void outputButton_Click(object sender, EventArgs e)
+        private void OutputButton_Click(object sender, EventArgs e)
         {
 
         }
