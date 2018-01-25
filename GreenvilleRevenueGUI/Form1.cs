4a87ab4ac1a2eacea3d2907ae461defe1ec4cdcd
Jeffrey Lindgren <kk4415un@my.lsc.edu>
2018-01-25 13:11:24 -0600

    Button code

--- GreenvilleRevenueGUI/Form1.cs
+++ GreenvilleRevenueGUI/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace GreenvilleRevenueGUI
 {
     public partial class greenvilleRevenueGUI : Form
@@ -24,6 +26,25 @@ namespace GreenvilleRevenueGUI
 
         private void outputButton_Click(object sender, EventArgs e)
         {
+            string numPeopleOne, numPeopleTwo;
+            int rev, num1, num2;
+
+            numPeopleOne = lastYearInputTextBox.Text;
+            num1 = Convert.ToInt32(numPeopleOne);
+
+            numPeopleTwo = thisYearInputTextBox.Text;
+            num2 = Convert.ToInt32(numPeopleTwo);
+
+            rev = num1 * 25;
+            RevenueOuput.Text = "Revenue for this year: " + rev.ToString("C");
+            RevenueOuput.Visible = true;
+            thisVsLastOutput.Visible = true;
+
+            if (num1 > num2)
+                thisVsLastOutput.Text = "Last year had more contestants than this year!";
+
+            else
+                thisVsLastOutput.Text = "This year has more contestants than last year";
 
         }
     }
